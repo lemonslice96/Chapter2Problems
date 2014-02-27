e076817895ab85f44f806016124b16a08cc45485
RyanClarker <clarke.ryan24@gmail.com>
2014-02-27 09:51:13 -0500

    New

--- Chapter2Problems/ConsoleApplication4/payroll.cs
+++ Chapter2Problems/ConsoleApplication4/payroll.cs
@@ -9,23 +9,27 @@ namespace ConsoleApplication4
     {
         static void Main(string[] args)
         {
-            string name, socialnumber, hoursworked, hourspayed;
+            string name, socialnumber;
+            double hourspay, hourswork, totalpay;
             
             Console.Write("Enter your name: ");
             name = Console.ReadLine();
             Console.Write("Social Security Number: ", name);
             socialnumber = Console.ReadLine();
             Console.Write("Hourly Pay Rate: ");
-            hourspayed = Console.ReadLine();
+            hourspay = Convert.ToDouble(Console.ReadLine());
             Console.Write("Hours Worked: ");
-            hoursworked = Console.ReadLine();
-
-            Console.Write("Payroll summary for: {0}.", name);
-            Console.Write("SSN: {0}.", socialnumber);
-            Console.Write("You earned $"{0} * hourspayed"
-
-      
-
+            hourswork = Convert.ToDouble(Console.ReadLine());
+            totalpay = hourspay * hourswork;
+            Console.WriteLine(" ");
+            Console.WriteLine("Payroll summary for {0}: ", name);
+            Console.WriteLine("SSN: {0}", socialnumber);
+            Console.WriteLine("You earned ${0} at ${1} per hour.", totalpay, hourspay);
+            Console.WriteLine(" ");
+            Console.WriteLine("Gross pay: {0, 18}.", totalpay);
+            Console.WriteLine("Feederal withholding: {0, 20}.", totalpay % 1.5f);
+            Console.WriteLine("State withholding: {0, 10}.", totalpay % 0.5f);
+            Console.WriteLine("Net pay: {0, 20}.", totalpay);
 
         }
     }
